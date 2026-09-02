1a3987c770bc39c3fcc03790332ff15c0f3d949b
Matthew Eshleman <matthew@covemountainsoftware.com>
2026-09-02 17:06:53 -0500

    PostAndProcess methods and associated unit tests.

--- cpputest-for-sst-lib/include/cms_cpputest_sst_ctrl.hpp
+++ cpputest-for-sst-lib/include/cms_cpputest_sst_ctrl.hpp
@@ -5,56 +5,64 @@
 #include <chrono>
 #include <cstdint>
 
-namespace cms::test::sst_ctrl {
+namespace cms::test::sst_ctrl
+{
+    enum : uint8_t
+    {
+        DUMMY_AO_A_PRIORITY,
+        DUMMY_AO_B_PRIORITY,
+        DUMMY_AO_C_PRIORITY,
+        DUMMY_AO_D_PRIORITY,
+        DUMMY_AO_E_PRIORITY,
+        UNIT_UNDER_TEST_PRIORITY
+    };
 
-enum : uint8_t {
-  DUMMY_AO_A_PRIORITY,
-  DUMMY_AO_B_PRIORITY,
-  DUMMY_AO_C_PRIORITY,
-  DUMMY_AO_D_PRIORITY,
-  DUMMY_AO_E_PRIORITY,
-  UNIT_UNDER_TEST_PRIORITY
-};
+    static_assert(UNIT_UNDER_TEST_PRIORITY < SST_PORT_MAX_TASK,
+                  "too many priorities defined");
 
-static_assert(UNIT_UNDER_TEST_PRIORITY < SST_PORT_MAX_TASK,
-              "too many priorities defined");
+    /**
+     * Setup for unit testing of an SST Task, using the faux SST host PC test.
+     * environment. Intended to be called from the cpputest setup() method.
+     * @param ticksPerSecond : ticks per second. Match your target device's setting.
+     * Default is 1000.
+     */
+    void Setup(uint32_t ticksPerSecond = 1000);
 
-/**
- * Setup for unit testing of an SST Task, using the faux SST host PC test.
- * environment. Intended to be called from the cpputest setup() method.
- * @param ticksPerSecond : ticks per second. Match your target device's setting.
- * Default is 1000.
- */
-void Setup(uint32_t ticksPerSecond = 1000);
+    /**
+     *  Reset, teardown.
+     *  Intended to be called from the cpputest teardown() method.
+     */
+    void Teardown();
 
-/**
- *  Reset, teardown.
- *  Intended to be called from the cpputest teardown() method.
- */
-void Teardown();
+    /**
+     * Process all events available for processing, in the same manner
+     * as the SST0 kernel.
+     *
+     * Returns when no more events are found.
+     */
+    void ProcessEvents();
 
-/**
- * Process all events available for processing, in the same manner
- * as the SST0 kernel.
- *
- * Returns when no more events are found.
- */
-void ProcessEvents();
+    /**
+     * Faux movement of time. Will execute the SST timer tick and
+     * also call ProcessEvents() with each tick.
+     * @param duration milliseconds of faux time to tick the kernel forward.
+     */
+    void MoveTimeForward(const std::chrono::milliseconds& duration);
 
-/**
- * Faux movement of time. Will execute the SST timer tick and
- * also call ProcessEvents() with each tick.
- * @param duration milliseconds of faux time to tick the kernel forward.
- */
-void MoveTimeForward(const std::chrono::milliseconds &duration);
+    inline void PostAndProcess(SST::Evt const* e, SST::Task* dest)
+    {
+        dest->post(e);
+        ProcessEvents();
+    }
 
-inline void PostAndProcess(SST::Evt const *e, SST::Task *dest) {
-  dest->post(e);
-  ProcessEvents();
-}
+    template <SST::Signal sig>
+    void PostAndProcess(SST::Task* dest)
+    {
+        static const SST::Evt constEvent = {sig};
+        PostAndProcess(&constEvent, dest);
+    }
 
-const char *GetVersion();
-
+    const char* GetVersion();
 } // namespace cms::test::sst_ctrl
 
 #endif // CPPUTEST_FOR_SST_LIB_CMS_CPPUTEST_SST_CTRL_HPP
--- cpputest-for-sst-lib/tests/cms_cpputest_sst_ctrl_post_tests.cpp
+++ cpputest-for-sst-lib/tests/cms_cpputest_sst_ctrl_post_tests.cpp
@@ -1,5 +1,6 @@
 #include "cms_cpputest_sst_ctrl.hpp"
 #include "sst.hpp"
+#include "cms_dummy_active_object.hpp"
 
 //cpputest header include must always be last
 #include "CppUTest/TestHarness.h"
@@ -8,9 +9,16 @@ using namespace cms::test;
 
 TEST_GROUP(sst_ctrl_post_tests)
 {
+    DefaultDummyActiveObjectUniquePtr dummy = nullptr;
+    std::array<SST::Evt const*, 10> events;
+
     void setup() final
     {
         sst_ctrl::Setup();
+        dummy = std::make_unique<DefaultDummyActiveObject>();
+        dummy->start(sst_ctrl::UNIT_UNDER_TEST_PRIORITY,
+                     events.data(), events.size(),
+                     nullptr);
     }
 
     void teardown() final
@@ -22,11 +30,26 @@ TEST_GROUP(sst_ctrl_post_tests)
 TEST(sst_ctrl_post_tests,
      provides_a_post_and_process_helper_func_with_trivial_signal_enum)
 {
-   //TODO
+    enum TestEnum : SST::Signal { TEST1_SIG = 100 };
+    SST::Signal capturedSig = 0xFFFF;
+    dummy->SetPostedEventHandler([&](const SST::Evt* e)
+    {
+        capturedSig = e->sig;
+    });
+    sst_ctrl::PostAndProcess<TEST1_SIG>(dummy.get());
+    CHECK_EQUAL(TEST1_SIG, capturedSig);
 }
 
 TEST(sst_ctrl_post_tests,
      provides_a_post_and_process_helper_func)
 {
-    //TODO
+    static constexpr SST::Signal TEST2_SIG = 111;
+    static constexpr SST::Evt testEvent{TEST2_SIG};
+    SST::Signal capturedSig = -1;
+    dummy->SetPostedEventHandler([&](const SST::Evt* e)
+    {
+        capturedSig = e->sig;
+    });
+    sst_ctrl::PostAndProcess(&testEvent, dummy.get());
+    CHECK_EQUAL(TEST2_SIG, capturedSig);
 }
